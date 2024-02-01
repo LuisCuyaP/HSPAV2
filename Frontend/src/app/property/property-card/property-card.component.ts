926554d3d5f4a223a42530fe8046a9a4286e6935
Enrique Cuya Puertas <luiscuyap@outlook.es>
2024-02-01 09:17:39 -0500

    Initial project

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent{
+
+  Property: any = {
+    "Id" : 1,
+    "Name": "DDDDD",
+    "Type": "House",
+    "Price": 12200
+
+  }
+
+
+}
