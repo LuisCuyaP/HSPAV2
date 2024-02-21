72988857f9c1139be432746a8ade34d46c15eefa
Enrique Cuya Puertas <luiscuyap@outlook.es>
2024-02-20 21:58:29 -0500

    se termina el addComponent registrando la info con localStorage

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -23,15 +23,24 @@ export class PropertyListComponent implements OnInit {
     //aca traigo las casas de tipo rent 2
     this.housingService.getAllProperties(this.SellRent).subscribe(
       data =>{
+        //aca obtengo las propiedades segun el tipo de sellrent, ya sea tipo 1 o 2
         this.properties = data;
-        const newProperty= JSON.parse(localStorage.getItem('newProp'));
 
-        //para ir agregando al componente  de buy/rent uno por uno cuando grabe un nuevo property
-        if(newProperty.SellRent === this.SellRent){
-          this.properties = [newProperty, ...this.properties];
-        }
+        //aca obtengo la nueva propiedad que acabo de guardar
+        //const newProperty= JSON.parse(localStorage.getItem('newProp'));
+
+        //si mi nueva propiedad que guarde es de tipo 1 y obviamente le digo que este sellrent sea de tipo uno antes de guardarlo en el add properti
+        //aca antes de llegar, cuando hago el submit defino el this.sellrent, si mi nueva propiedad es de tipo 1 o 2 en mi on submit le dire que this.sellrente se aca de agrega un tipo 1 o 2
+        //if(newProperty.SellRent === this.SellRent){
+          //entonces aca agrego la nueva propiedad, y como en mi html lo abasteco con properties entonces me mostrara el json con la nueva propiedad ingresada
+        //  this.properties = [newProperty, ...this.properties];
+        //}
+
+
+
 
         console.log(this.route.snapshot.url.toString());
+        console.log(data);
       }, error => {
         console.log(error);
       }
--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -16,6 +16,18 @@ export class HousingService {
     return this.http.get('data/properties.json').pipe(
       map(data => {
         const propertiesArray: Array<IProperty> = [];
+        const localProperties = JSON.parse(localStorage.getItem('newProp'));
+
+        if(localProperties){
+          for(const id in localProperties){
+            if(localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent){
+              propertiesArray.push(localProperties[id]);
+            }
+          }
+        }
+
+
+
         for(const id in data){
           if(data.hasOwnProperty(id) && data[id].SellRent === SellRent){
             propertiesArray.push(data[id]);
@@ -24,10 +36,31 @@ export class HousingService {
         return propertiesArray;
       })
     );
+    
   }
 
   addProperty(property: Property) {
-    localStorage.setItem('newProp', JSON.stringify(property));
+    //aca lo convierto en un array
+    let newProp = [property];
+
+    //si agrego una nueva propiedad en el array , y su newProp ya existe entonces agregalo al array
+    if(localStorage.getItem('newProp')){
+      newProp = [property,
+                  ...JSON.parse(localStorage.getItem('newProp'))];      
+    }
+
+    localStorage.setItem('newProp', JSON.stringify(newProp));
   }
+
+  newPropId(){
+    if(localStorage.getItem('PID')){
+      localStorage.setItem('PID', String(+localStorage.getItem('PID') + 1));
+      return +localStorage.getItem('PID');
+    }else{
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
+  }
+  
   
 }
